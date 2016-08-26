c19294df23d40ba0d05fbbdc11d848c617a51600
John0king <AblueSky@outlook.com>
2016-08-26 12:11:26 +0800

    改为netStandard 1.1

--- src/CoreVerify/Color/RGB.cs
+++ src/CoreVerify/Color/RGB.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace KAdmin.KVerifyCode.Color
+namespace CoreVerify.Color
 {
     public class RGB
     {
--- src/CoreVerify/Font/Internal/NumberFonts.cs
+++ src/CoreVerify/Font/Internal/NumberFonts.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace KAdmin.KVerifyCode.Font.Internal
+namespace CoreVerify.Font.Internal
 {
     public class NumberFonts:IBitFont
     {
